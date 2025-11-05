c39c883765d8977d1447d25e34631da698425ff9
Mikko Kujala <mikko@unicornmachine.com>
2025-11-05 13:34:04 +0200

    Forgot to add these

---
+++ src/modules/media/components/MediaModuleShell.tsx
@@ -0,0 +1,39 @@
+import { useMemo, useSyncExternalStore } from 'react';
+import { createPortal } from 'react-dom';
+import { RoomContext } from '@livekit/components-react';
+import type { ViewProps } from 'src/client/user-interface';
+import type { MediaModuleClient } from '../client';
+import { MediaModuleProvider } from '../context/MediaModuleContext';
+import { MEDIA_PORTAL_SLOTS } from './MediaPortalAnchors';
+import { MediaContextItem } from './MediaContextItem';
+import { TakePictureButton } from './TakePictureButton';
+import { RecordButton } from './RecordButton';
+import { WebCam } from './WebCam';
+
+export const MediaModuleShell: React.FC<ViewProps<MediaModuleClient>> = ({ module }) => {
+    const version = useSyncExternalStore(
+        (listener) => module.subscribeUpdates(listener),
+        () => module.getVersion(),
+        () => module.getVersion(),
+    );
+
+    const contextValue = useMemo(
+        () => module.getMediaContextValue(),
+        [module, version],
+    );
+
+    const takePictureTarget = module.getPortalTarget(MEDIA_PORTAL_SLOTS.footerTakePicture);
+    const recordTarget = module.getPortalTarget(MEDIA_PORTAL_SLOTS.footerRecord);
+    const webCamTarget = module.getPortalTarget(MEDIA_PORTAL_SLOTS.footerWebcam);
+
+    return (
+        <MediaModuleProvider value={contextValue}>
+            <RoomContext.Provider value={contextValue.room}>
+                <MediaContextItem module={module} />
+                {takePictureTarget && createPortal(<TakePictureButton module={module} />, takePictureTarget)}
+                {recordTarget && createPortal(<RecordButton module={module} />, recordTarget)}
+                {webCamTarget && createPortal(<WebCam />, webCamTarget)}
+            </RoomContext.Provider>
+        </MediaModuleProvider>
+    );
+};
