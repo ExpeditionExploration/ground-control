47d4924230320f2e7f4bb11237d0d9376df89ffe
Joonas Holmi <jtholmi@gmail.com>
2025-11-13 10:54:23 +0200

    Enable needed modules

--- src/modules/server.ts
+++ src/modules/server.ts
@@ -1,22 +1,22 @@
 // The controller should be exported as a namespaced object.
 import { StatsModuleServer } from './stats/server';
 import { MediaModuleServer } from './media/server';
-// import { LightsModuleServer } from './lights/server';
+import { LightsModuleServer } from './lights/server';
 import { ControlModuleServer } from './control/server';
-// import { AngleModuleServer } from './angle/server';
+import { AngleModuleServer } from './angle/server';
 import { SpatialModuleServer } from './spatial/server';
 import { IMUModuleServer } from './imu/server';
-// import { BatteryModuleServer } from './battery/server';
+import { BatteryModuleServer } from './battery/server';
 // import { TOFModuleServer } from './tof/server';
 
 export const modules = new Map([
     ['stats', StatsModuleServer],
     ['media', MediaModuleServer],
-    // ['lights', LightsModuleServer],
+    ['lights', LightsModuleServer],
     ['control', ControlModuleServer],
     ['imu', IMUModuleServer],
-    // ['angle', AngleModuleServer],
+    ['angle', AngleModuleServer],
     ['spatial', SpatialModuleServer],
-    // ['battery', BatteryModuleServer],
+    ['battery', BatteryModuleServer],
     // ['tof', TOFModuleServer],
 ]);
