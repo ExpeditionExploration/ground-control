67a8c75b845d652e6ef1bea1772e4e54b945ef19
Joonas Holmi <jtholmi@gmail.com>
2025-11-13 10:51:15 +0200

    Enable needed modules

--- src/modules/client.ts
+++ src/modules/client.ts
@@ -1,20 +1,20 @@
 // The controller should be exported as a namespaced object.
 import { StatsModuleClient } from './stats/client';
 import { MediaModuleClient } from './media/client';
-// import { AngleModuleClient } from './angle/client';
+import { AngleModuleClient } from './angle/client';
 import { ControlModuleClient } from './control/client';
 import { SpatialModuleClient } from './spatial/client';
 import { BatteryModuleClient } from './battery/client';
-// import { LightsModuleClient } from './lights/client';
-// import { TOFModuleClient } from './tof/client';
+import { LightsModuleClient } from './lights/client';
+import { TOFModuleClient } from './tof/client';
 
 export const modules = new Map([
     ['spatial', SpatialModuleClient],   
     ['stats', StatsModuleClient],
     ['media', MediaModuleClient],
-    // ['angle', AngleModuleClient],
+    ['angle', AngleModuleClient],
     ['control', ControlModuleClient],
     ['battery', BatteryModuleClient],
-    // ['lights', LightsModuleClient],
-    // ['tof', TOFModuleClient],
+    ['lights', LightsModuleClient],
+    ['tof', TOFModuleClient],
 ]);
